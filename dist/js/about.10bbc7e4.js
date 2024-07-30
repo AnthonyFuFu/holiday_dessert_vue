53ac264f2a8ef7a32a2f447dd5577f208646d4e9
AnthonyFuFu <s9017688@yahoo.com.tw>
2024-07-30 14:44:38 +0800

    add dist & node_modules

---
+++ dist/js/about.10bbc7e4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkholiday_dessert"]=self["webpackChunkholiday_dessert"]||[]).push([[594],{603:function(e,n,s){s.r(n),s.d(n,{default:function(){return l}});var t=s(6768);const u={class:"about"},a=(0,t.Lk)("h1",null,"This is an about page",-1),r=[a];function c(e,n){return(0,t.uX)(),(0,t.CE)("div",u,r)}var i=s(1241);const o={},d=(0,i.A)(o,[["render",c]]);var l=d}}]);
+//# sourceMappingURL=about.10bbc7e4.js.map
--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,21 @@ const fs = require('fs')
 
 module.exports = {
     devServer: {
-        https: {
-          key: fs.readFileSync('./certs/private.key'),
-          cert: fs.readFileSync('./certs/certificate.crt'),
-        },
-        public: 'https://your domain or IP:8080/',
-        disableHostCheck: true
+
+        // 測試機
+        allowedHosts: 'all', // or specify specific hosts if needed
+        https: false, // Enable HTTPS if needed
+        host: 'localhost', // or '0.0.0.0' for external access
+        port: 8080, // Default port or change to your preferred port
+        open: true, // Automatically open the browser
+
+        // 正式機
+        // https: {
+        //   key: fs.readFileSync('./certs/private.key'),
+        //   cert: fs.readFileSync('./certs/certificate.crt'),
+        // },
+        // public: 'https://your domain or IP:8080/',
+        // disableHostCheck: true
+
     }
 }
